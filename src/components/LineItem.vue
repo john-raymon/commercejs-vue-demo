9315ded6bbc90e5d34d4b3d85c06dd0157cb4ac7
John Ray <john@johnraymon.com>
2019-06-24 18:56:56 -0400

    create src/components/LineItem.vue: implement LineItem SFC, emit event on click of button 'remove from cart'

---
+++ src/components/LineItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="cart-container__line-item w-100 flex items-center justify-between pb2">
+    <div class="w-auto pr1">
+      <p class="tracked ttc f7">
+        {{ `${item.name} - ${item.quantity} ${item.line_total.formatted_with_code}` }}
+      </p>
+    </div>
+    <button class="dim b1 b--mid-gray outline-0 pointer pa2" @click="$emit('remove-item-from-cart', item.id)">
+      Remove from Cart
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LineItem",
+  props: [
+    'item'
+  ]
+}
+</script>

de3eb6c0ae1492f06f52b45f1335b11c39a5aefa
John Ray <john@johnraymon.com>
2019-06-24 16:32:59 -0400

    update src/App.vue: remove style tags, import application scss, implement markup with logic to render multiple ProductItems. set initial data.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="App ph2 pv4">
+    <fragment v-if="!checkout">
+      <div class="products-container mw7 center cf">
+        <h2 class="tracked ttu gray">
+          All Products
+        </h2>
+        <fragment v-if="products.length">
+          <product-item v-for="product of products" :product="product" :key="product.id">
+          </product-item>
+        </fragment>
+        <p v-else>There no products available right now</p>
+      </div>
+    </fragment>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// components
+import ProductItem from './components/ProductItem'
+// vue-fragment
+import { Fragment } from 'vue-fragment'
+import './styles/application.scss'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Fragment,
+    ProductItem
+  },
+  props: {
+    commerce: {
+      required: true,
+      type: Object
+    }
+  },
+  created: function() {
+    if (this.commerce !== undefined && typeof this.commerce !== 'undefined') {
+      // invoke commerce products method to get all products
+      this.commerce.Products.list(
+        (resp) => {
+          //Success
+          this.products = resp.data;
+        },
+        (error) => {
+          // handle error properly in real-world
+        }
+      );
+      window.addEventListener("Commercejs.Cart.Ready", function () {
+      // invoke commerce cart method to retrieve cart in session
+        this.commerce.Cart.retrieve((cart) => {
+          if (!cart.error) {
+            this.cart = cart
+          }
+        });
+      }.bind(this))
+    }
+  },
+  data: function() {
+    return {
+      products: [],
+      cart: null,
+      checkout: null,
+      order: null
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>

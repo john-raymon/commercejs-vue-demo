2de662b98d35cec70dfc7c7de8c6917c7022d393
John Ray <john@johnraymon.com>
2019-06-24 19:00:56 -0400

    create src/components/Cart.vue: implement Cart SFC, re-emit 'remove-item-from-cart' from component, after catching it from line-item component.

---
+++ src/components/Cart.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="mw7 center">
+    <div v-if="cart" class="cart-container center">
+        <h2 class="tracked ttu gray flex justify-between">
+          Cart ({{cart.total_items}})
+          <span class="flex flex-column f7">
+            <span>
+              subtotal: ${{cart.subtotal.formatted_with_code}}
+            </span>
+          </span>
+        </h2>
+        <ul v-if="cart.total_items > 0" class="w-100 list-reset ma0 pv0 ph4">
+          <line-item v-for="item of cart.line_items" :item="item" @remove-item-from-cart="$emit('remove-item-from-cart', $event)"/>
+        </ul>
+        <p v-else class="f7 tracked">Your cart is empty</p>
+    </div>
+    <p v-else class="center">
+      Loading Cart...
+    </p>
+  </div>
+</template>
+
+<script>
+import LineItem from './LineItem'
+export default {
+  name: 'cart',
+  props: ['cart'],
+  components: {
+    LineItem
+  }
+}
+</script>

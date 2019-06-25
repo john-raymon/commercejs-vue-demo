5d0833fd0ccf964f6c38a4a8b7adbbeb6b228ff5
John Ray <john@johnraymon.com>
2019-06-25 15:40:58 -0400

    create src/components/CheckoutItem.vue: implement component to render as item for checkout

---
+++ src/components/CheckoutItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="cart-container__line-item w-100 flex items-center justify-between pb2">
+    <div class="w-auto pr1">
+      <p class="tracked ttc f7">
+        {{item.product_name}} - ({{item.quantity}})
+      </p>
+    </div>
+    <p class="pa0 ma0">
+      ${{item.line_total.formatted_with_code}}
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'CheckoutItem',
+  props: [
+    'item'
+  ]
+}
+</script>

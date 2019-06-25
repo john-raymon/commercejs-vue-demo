4ebf330dc0e4401b9326361cdf8700e71b0212b7
John Ray <john@johnraymon.com>
2019-06-25 15:41:50 -0400

    update src/components/Cart.vue: add key to line-items implement button to checkout

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -10,9 +10,17 @@
           </span>
         </h2>
         <ul v-if="cart.total_items > 0" class="w-100 list-reset ma0 pv0 ph4">
-          <line-item v-for="item of cart.line_items" :item="item" @remove-item-from-cart="$emit('remove-item-from-cart', $event)"/>
+          <line-item v-for="item of cart.line_items" :key="item.id" :item="item" @remove-item-from-cart="$emit('remove-item-from-cart', $event)"/>
         </ul>
         <p v-else class="f7 tracked">Your cart is empty</p>
+
+        <button
+          class='dim b1 outline-0 pointer pa2 mt2 db w-100'
+          :class="[cart.total_items === 0 ? 'b--light-gray' : 'b--mid-gray' ]"
+          :disabled="(cart.total_items === 0)"
+          @click="$emit('create-checkout')">
+          Checkout
+        </button>
     </div>
     <p v-else class="center">
       Loading Cart...
